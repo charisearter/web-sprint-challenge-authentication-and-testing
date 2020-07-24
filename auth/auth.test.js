bf8df6e2ce361fe8a725d40457cb7346f3750f49
Charise Arter <mine@Charises-MacBook-Pro.local>
2020-07-24 12:27:08 -0500

    all pass MVP

--- auth/auth.test.js
+++ auth/auth.test.js
@@ -1,14 +1,63 @@
 //import what I am testing
 const auth = require("./auth-router.js");
+const supertest = require("supertest");
+const server = require("../api/server");
+
+const users = [
+	{
+		username: "user1",
+		password: "abcde",
+	},
+	{
+		username: "user2",
+		password: "abcd",
+	},
+	{
+		username: "user1",
+		password: "Abcd",
+	},
+];
 
 //tests
 
 describe("Register tests", () => {
-	it("user can register", () => {});
-	it("user cannot register without password", () => {});
+	//passes only fails because needs to be unique username each time
+	it("user can register", () => {
+		return supertest(server)
+			.post("/api/auth/register")
+			.send({ username: "user5", password: "asdf" })
+			.then((res) => {
+				expect(res.status).toBe(201);
+			});
+	});
+	//passes
+	it("username is not unique, res 500", () => {
+		return supertest(server)
+			.post("/api/auth/register")
+			.send(users[0])
+			.then((res) => {
+				expect(res.status).toBe(500);
+			});
+	});
 });
 
 describe("Login tests", () => {
-	it("successful login, res 200", () => {});
-	it("failed login, res 404", () => {});
+	//passes
+	it("successful login, res 200", () => {
+		return supertest(server)
+			.post("/api/auth/login")
+			.send(users[0])
+			.then((res) => {
+				expect(res.status).toBe(200);
+			});
+	});
+	it("failed login wrong password, res 401", () => {
+		return supertest(server)
+			.post("/api/auth/login")
+			.send(users[2])
+			.then((res) => {
+				expect(res.status).toBe(401);
+				expect(res.type).toMatch(/json/i);
+			});
+	});
 });
